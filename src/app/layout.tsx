f11937e97e65ac84a0b7e022ebbb33d82537c59b
uei91 <wkwk2805@amicuslex.net>
2024-10-08 17:38:31 +0900

    Feat: create map

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,12 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="en">
+      <head>
+        <script
+          type="text/javascript"
+          src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}`}
+        ></script>
+      </head>
       <body>{children}</body>
     </html>
   );
---
+++ window.d.ts
@@ -0,0 +1,3 @@
+interface Window {
+  naver: typeof naver;
+}
