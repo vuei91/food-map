1d0549d3e50b07ef3b72c42bf1b9ada2a15b1661
uei91 <wkwk2805@amicuslex.net>
2024-10-08 17:43:24 +0900

    Refact: 컴포넌트화

---
+++ src/app/components/NaverMap.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+
+const NaverMap = () => {
+  const mapElement = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.naver && mapElement.current) {
+      const mapOptions = {
+        center: new window.naver.maps.LatLng(37.5665, 126.978), // 서울 중심 좌표
+        zoom: 10, // 줌 레벨
+      };
+
+      // 지도 생성 및 렌더링
+      const map = new window.naver.maps.Map(mapElement.current, mapOptions);
+
+      // 마커 추가 예시
+      new window.naver.maps.Marker({
+        position: new window.naver.maps.LatLng(37.5665, 126.978),
+        map: map,
+      });
+    }
+  }, []);
+  return <div ref={mapElement} className="w-full h-screen"></div>;
+};
+
+export default NaverMap;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,8 @@
-"use client";
 import React, { useEffect, useRef } from "react";
+import NaverMap from "./components/NaverMap";
 
-const Hello = () => {
-  const mapElement = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    if (typeof window !== "undefined" && window.naver && mapElement.current) {
-      const mapOptions = {
-        center: new window.naver.maps.LatLng(37.5665, 126.978), // 서울 중심 좌표
-        zoom: 10, // 줌 레벨
-      };
-
-      // 지도 생성 및 렌더링
-      const map = new window.naver.maps.Map(mapElement.current, mapOptions);
-
-      // 마커 추가 예시
-      new window.naver.maps.Marker({
-        position: new window.naver.maps.LatLng(37.5665, 126.978),
-        map: map,
-      });
-    }
-  }, []);
-  return <div ref={mapElement} className="w-full h-screen"></div>;
+const Home = () => {
+  return <NaverMap />;
 };
 
-export default Hello;
+export default Home;
