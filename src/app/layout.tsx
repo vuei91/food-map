71f0a1f59772df14bf87138c50599a8788804119
uei91 <wkwk2805@amicuslex.net>
2024-10-10 16:41:32 +0900

    Feat: 다시처리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,10 +14,10 @@ const RootLayout = ({
   return (
     <html lang="en">
       <head>
-        <Script
-          strategy="lazyOnload"
+        <script
+          type="text/javascript"
           src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}`}
-        />
+        ></script>
       </head>
       <body>{children}</body>
     </html>
